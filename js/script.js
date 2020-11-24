174582325608e7ef66934e5474bf9f5e879821f3
psheka <pshekaprod@gmail.com>
2020-11-24 23:09:28 +0300

    loops & conditional

--- js/script.js
+++ js/script.js
@@ -1,26 +1,17 @@
 "use strict"; /* сторгий режим(не позволяет использовать устаревшие функции и тд) */
 
 /* Задание на урок:
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-Проверить, чтобы все работало без ошибок в консоли */
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
+str.length - и получить её длину)
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+4) Потренироваться и переписать цикл еще двумя способами*/
 
 const _numberOfFilms = +prompt("How many films did u see?",'');
-
 
 
 
@@ -32,18 +23,45 @@ let personalMovieDB = {
     privat:false
 };
 
-const _lastMovie = prompt("what is the last film did u see?",''),
-      _Rate = +prompt("how did u rate it from 0 to 10",''),
-      secMovie = prompt("what is the last film did u see?",''),
-      secRate = +prompt("how did u rate it from 0 to 10",'');
 
-personalMovieDB.movies = {
-[_lastMovie]:_Rate,
-[secMovie]:secRate
-};
+/* for(let i = 0;i<2;i++){
+    
+    let _lastMovie = prompt("what is the last film did u see?",''),
+        _Rate = +prompt("how did u rate it from 0 to 10",'');
 
+    if ((_lastMovie.length<1||_lastMovie.length>50)||(_Rate<0||_Rate>10)||isNaN(_Rate)){
+        alert('данные введены не коректно, введите еще раз');
+        i--;
+    }else{
+        personalMovieDB.movies[_lastMovie] = _Rate;
+    }
+
+        
+} */
+let i = 0;
+while(i<2){
+    let _lastMovie = prompt("what is the last film did u see?",''),
+        _Rate = +prompt("how did u rate it from 0 to 10",'');
+
+    if ((_lastMovie.length<1||_lastMovie.length>50)||(_Rate<0||_Rate>10)||isNaN(_Rate)){
+        alert('данные введены не коректно, введите еще раз');
+        
+    }else{
+        personalMovieDB.movies[_lastMovie] = _Rate;
+        i++;
+    }
+}
 
 
 
 console.log(personalMovieDB);
+if (personalMovieDB.count<10){
+    console.log("Просмотрено довольно мало фильмовь");
+}else if(personalMovieDB.count>30){
+    console.log("Вы киноман");
+}else if (personalMovieDB.count<=30||personalMovieDB.count>=10){
+    console.log("Вы классический зритель");
+}else{
+    console.log("Произошла ошибка");
+}
 
