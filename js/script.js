97185900e57b044965ee26ebcc2723a9e4eded1f
psheka <pshekaprod@gmail.com>
2020-11-29 19:25:25 +0300

    add counter function

--- js/script.js
+++ js/script.js
@@ -11,11 +11,11 @@ P.S. Функции вызывать не обязательно */
 
 let _numberOfFilms;
 
-function start(){
-     _numberOfFilms = +prompt("How many films did u see?",'');
-     while(_numberOfFilms == '' || _numberOfFilms == null || isNaN(_numberOfFilms)){
-        _numberOfFilms = +prompt("How many films did u see?",'');
-     }
+function start() {
+    _numberOfFilms = +prompt("How many films did u see?", '');
+    while (_numberOfFilms == '' || _numberOfFilms == null || isNaN(_numberOfFilms)) {
+        _numberOfFilms = +prompt("How many films did u see?", '');
+    }
 }
 
 start();
@@ -23,13 +23,14 @@ start();
 
 
 
-                let personalMovieDB = {
-                    count: _numberOfFilms,
-                    movies: {},
-                    actors: {},
-                    genres: [],
-                    privat:false
-                };
+let personalMovieDB = {
+    count: _numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
 
 
 /* for(let i = 0;i<2;i++){
@@ -48,61 +49,66 @@ start();
 } */
 
 
-function rememberMyFilms(){
+function rememberMyFilms() {
 
     let i = 0;
-    while(i<2){
-        let _lastMovie = prompt("what is the last film did u see?",''),
-            _Rate = +prompt("how did u rate it from 0 to 10",'');
+    while (i < 2) {
+        let _lastMovie = prompt("what is the last film did u see?", ''),
+            _Rate = +prompt("how did u rate it from 0 to 10", '');
 
-        if ((_lastMovie==null||_lastMovie.length<1||_lastMovie.length>50)||
-            (_Rate==null||_Rate<0||_Rate>10)||isNaN(_Rate)){
-                
+        if ((_lastMovie == null || _lastMovie.length < 1 || _lastMovie.length > 50) ||
+            (_Rate == null || _Rate < 0 || _Rate > 10) || isNaN(_Rate)) {
+
             alert('данные введены не коректно, введите еще раз');
-            
-        }else{
+
+        } else {
             personalMovieDB.movies[_lastMovie] = _Rate;
             i++;
         }
-    }  
+    }
 }
 
 //rememberMyFilms();
 
 
 
-function detectPersonalLevel(){
+function detectPersonalLevel() {
 
-    if (personalMovieDB.count<10){
+    if (personalMovieDB.count < 10) {
         console.log("Просмотрено довольно мало фильмовь");
-    }else if(personalMovieDB.count>30){
+    } else if (personalMovieDB.count > 30) {
         console.log("Вы киноман");
-    }else if (personalMovieDB.count<=30||personalMovieDB.count>=10){
+    } else if (personalMovieDB.count <= 30 || personalMovieDB.count >= 10) {
         console.log("Вы классический зритель");
-    }else{
+    } else {
         console.log("Произошла ошибка");
-    }    
+    }
 }
 
 //detectPersonalLevel();
 
-function showMyDB(hidden){
+function showMyDB(hidden) {
 
-    if (!hidden){
+    if (!hidden) {
         console.log(personalMovieDB);
-    }else{
+    } else {
         console.log('данные скрыты');
     }
 }
 showMyDB(personalMovieDB.privat);
 
-function writeYourGenres(){
-    
-    for (let i = 0;i<3;i++)
-    {
-        let c=i+1;
-        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${c}`);
+function writeYourGenres() {
+
+    for (let i = 1; i < 4; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
     }
 }
 
 writeYourGenres();
+
+function objectCounter(obj) {
+
+    console.log((Object.keys(obj)).length);
+}
+
+objectCounter(personalMovieDB);
